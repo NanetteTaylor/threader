36bba6791cff56278ef22131d7c4a10520a5ecbd
nanettetaylor <nanette.m.taylor@gmail.com>
2020-06-24 19:08:28 +0000

    Setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Threader</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
