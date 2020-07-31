7af13a5a590fb5ac8cd1d4a9bb32a81a0848ce62
nanettetaylor <nanette.m.taylor@gmail.com>
2020-07-31 10:50:40 +0000

    Still working on index route

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(passport.session());
 
 app.get("/", function(req, res, next) {
   // res.send("Access the API at path /api");
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
@@ -110,7 +110,7 @@ app.get('/twitter-callback',
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
