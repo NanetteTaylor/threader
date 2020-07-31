0daa28a9f4d3323ae2b7c6b8f505a167eec49a31
nanettetaylor <nanette.m.taylor@gmail.com>
2020-07-31 10:59:18 +0000

    Rerun build

---
+++ build/precache-manifest.1c01b498835c57b9d01149823a69c950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e0bb2def1c65021bdd3081e99b8ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80b419a117a1ff1abe4",
+    "url": "/static/css/main.c4050355.chunk.css"
+  },
+  {
+    "revision": "0bec567689fa4b7c8cda",
+    "url": "/static/js/2.70f33f06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70f33f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80b419a117a1ff1abe4",
+    "url": "/static/js/main.0fdaf7c4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
