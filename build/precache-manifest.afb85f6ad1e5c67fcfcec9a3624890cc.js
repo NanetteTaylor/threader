d70b0542c796caf23dc6fd86f0e5c715d274c810
nanettetaylor <nanette.m.taylor@gmail.com>
2020-07-31 10:43:50 +0000

    Figuring out routes

---
+++ build/precache-manifest.afb85f6ad1e5c67fcfcec9a3624890cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd44812cbe381ce9bbb3f16e1c306ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12fa04612cbb481fec8d",
+    "url": "/static/css/main.c4050355.chunk.css"
+  },
+  {
+    "revision": "0bec567689fa4b7c8cda",
+    "url": "/static/js/2.70f33f06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70f33f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fa04612cbb481fec8d",
+    "url": "/static/js/main.e6efa402.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
